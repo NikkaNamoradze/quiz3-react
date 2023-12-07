a1cc921d22673ba9cde44c00ec9a1db818bdfd6c
NikkaNamoradze <107554272+NikkaNamoradze@users.noreply.github.com>
2023-12-07 12:34:47 +0400

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
 
-function App() {
+const App = () => {
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/photos?_limit=4');
+        setPhotos(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <PhotoList>
+        {photos.map(photo => (
+          <PhotoItem key={photo.id}>
+            <PhotoImage src={photo.thumbnailUrl} alt={photo.title} />
+            <PhotoTitle>{photo.title}</PhotoTitle>
+          </PhotoItem>
+        ))}
+      </PhotoList>
+    </Container>
   );
-}
+};
 
 export default App;
+
+const Container = styled.div`
+  text-align: center;
+`;
+
+const PhotoList = styled.ul`
+  list-style: none;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
+
+const PhotoItem = styled.li`
+  flex: 0 1 30%;
+  max-width: 300px;
+  margin: 1.5%;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px;
+`;
+
+const PhotoTitle = styled.p`
+  margin-top: 8px;
+  font-weight: 400;
+`;
+
+const PhotoImage = styled.img`
+  width: 150px;
+  height: 150px;
+  object-fit: cover;
+`;
